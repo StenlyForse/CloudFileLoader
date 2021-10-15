50331c102e77d2357b181c8cb99c67f4f0ac2242
Kurlin Alexandr <sasha-kurlin@mail.ru>
2021-10-15 17:01:04 +0300

    Initialized

---
+++ CloudFileLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CloudFileLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
